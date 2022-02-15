b32abd78fa38587aafc8d008b7161d1d5705963b
Willy <jemand771@gmx.net>
2022-02-15 21:45:03 +0100

    add station names as label

--- collectors/tankerk.py
+++ collectors/tankerk.py
@@ -12,19 +12,34 @@ class TankerKCollector(CollectorBase):
         super().__init__(env)
         self.token = self.env_get_raise("API_KEY")
         id_list = self.env_get_raise("ID_LIST")
-        self.ids = id_list.replace(",", " ").split()
+        ids = id_list.replace(",", " ").split()
+        self.id_to_name = {}
+        for id_ in ids:
+            self.id_to_name[id_] = self.request_name(id_)
 
     def collect(self):
-        g = GaugeMetricFamily("price", "fuel price", labels=["station_id", "fuel_type"])
-        for id_, prices in self.request_prices().items():
+        g = GaugeMetricFamily("price", "fuel price", labels=["station_id", "station_name", "fuel_type"])
+        for id_, prices in self.request_prices(list(self.id_to_name)).items():
             for fuel_type, value in prices.items():
-                g.add_metric([id_, fuel_type], value)
+                g.add_metric([id_, self.id_to_name[id_], fuel_type], value)
         yield g
 
-    def request_prices(self):
+    def request_name(self, id_):
+        r = requests.get(
+            f"{self.BASE_URL}/detail.php",
+            params={
+                "id": id_,
+                "apikey": self.token
+            }
+        )
+        assert r.status_code == 200
+        assert r.json().get("status") == "ok"
+        return r.json().get("station").get("name")
+
+    def request_prices(self, ids):
         prices = {}
-        for i in range(len(self.ids)):
-            target_ids = self.ids[i:i + 10]
+        for i in range(len(ids)):
+            target_ids = ids[i:i + 10]
             r = requests.get(
                 f"{self.BASE_URL}/prices.php",
                 params={
@@ -32,8 +47,8 @@ class TankerKCollector(CollectorBase):
                     "apikey": self.token
                 }
             )
-            if not r.json().get("ok"):
-                continue
+            assert r.status_code == 200
+            assert r.json().get("ok")
             for id_, data in r.json().get("prices").items():
                 if data.get("status") != "open":
                     continue
